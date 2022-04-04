dbcbd6c7251a2b5d57dbd169078e8d477a8b41e0
Fernando Romano Batista <fernando.batista@hbsis.com.br>
2022-04-04 13:43:05 -0300

    adding stock repository as a httpClient

--- HelloChatApp.BotConsole/Program.cs
+++ HelloChatApp.BotConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HelloChatApp.BotConsole.Abstractions;
 using HelloChatApp.BotConsole.Consumers;
 using HelloChatApp.BotConsole.Infra.RabbitMq;
+using HelloChatApp.BotConsole.Infra.Services;
 using HelloChatApp.BotConsole.Messages;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,7 +11,8 @@ var builder = Host.CreateDefaultBuilder(args);
 builder.ConfigureServices(services =>
 {
     services.AddScoped<IConsumer<StockQueryCommand>, StockQueryCommandConsumer>();
-    services.AddSingleton<IRabbitMqConnection, RabbitMqConnection>();    
+    services.AddSingleton<IRabbitMqConnection, RabbitMqConnection>();
+    services.AddHttpClient<IStockRepository, StockRepository>();
 });
 
 var app = builder.Build();
